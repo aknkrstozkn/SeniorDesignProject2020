1f83493617430310f32e0d397c9dc5e34ae8a374
emrsmsrli <emrsmsrli1@gmail.com>
2020-04-04 15:55:33 +0300

    implement opencamera fragment

--- app/src/main/java/com/example/seniordesignproject2020/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/seniordesignproject2020/ui/home/HomeFragment.java
@@ -4,32 +4,65 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
 
 import com.example.seniordesignproject2020.R;
 
 public class HomeFragment extends Fragment {
-
-    private HomeViewModel homeViewModel;
+    ImageButton openCameraButton = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        openCameraButton = root.findViewById(R.id.open_camera);
+        openCameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                // todo Intent openCameraIntent = new Intent(getContext(), TakePicture.class);
+                // todo getActivity().startActivity(openCameraIntent);
             }
         });
+
+        scaleUp();
         return root;
+    }
+
+    @Override
+    public void onStop() {
+        openCameraButton.clearAnimation();
+        super.onStop();
+    }
+
+    private void doScale(float scale, Runnable onEnd) {
+        openCameraButton.animate()
+                .scaleX(scale)
+                .scaleY(scale)
+                .setDuration(400)
+                .setInterpolator(new FastOutSlowInInterpolator())
+                .withEndAction(onEnd)
+                .start();
+    }
+
+    private void scaleUp() {
+        doScale(1.2f, new Runnable() {
+            @Override
+            public void run() {
+                scaleDown();
+            }
+        });
+    }
+
+    private void scaleDown() {
+        doScale(1.0f, new Runnable() {
+            @Override
+            public void run() {
+                scaleUp();
+            }
+        });
     }
 }
